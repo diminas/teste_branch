10dac574558b1aa46f7a69d0cdf785da30f80774
ibellyve <cibellymendes616729@outlook.com>
2023-09-04 17:10:20 -0300

    Front att

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<!-- comment -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+        body{
+            backgroud-color:pink;}
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>alterando para front</title>
 </head>
 <body>
-    
+    <h2>Subtitulo do teste</h2>
 </body>
+<script></script>
 </html>
